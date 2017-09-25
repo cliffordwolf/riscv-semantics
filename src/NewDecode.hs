62b706d98c160939679ae41b5c1ee6a3667dd889
Clifford Wolf <clifford@clifford.at>
2017-09-25 23:39:41 +0200

    Improvements in NewDecode.hs

--- src/NewDecode.hs
+++ src/NewDecode.hs
@@ -7,21 +7,27 @@ import Data.Maybe
 -----------------------------------------
 -- Mockup for new decode
 
+data NewInstruction =
+  NewAddi { len :: Integer, rd :: Register, rs1 :: Register, imm12 :: Integer } |
+  NewSlti { len :: Integer, rd :: Register, rs1 :: Register, imm12 :: Integer } |
+  NewSltiu { len :: Integer, rd :: Register, rs1 :: Register, imm12 :: Integer }
+  deriving (Eq, Read, Show)
+
 getOpcode inst = fromIntegral $ bitSlice inst 0 7
 getFunct3 inst = fromIntegral $ bitSlice inst 12 15
 
-newDecode :: [Word8] -> (Maybe Instruction, [Word8])
+newDecode :: [Word8] -> Maybe NewInstruction
 
-newDecode (b0:b1:b2:b3:rest)
-    | (getOpcode inst) == 0x13 && (getFunct3 inst) == 0 = (Just $ Addi (getRd inst) (getRs1 inst) (getImm12 inst), rest)
-    | (getOpcode inst) == 0x13 && (getFunct3 inst) == 2 = (Just $ Slti (getRd inst) (getRs1 inst) (getImm12 inst), rest)
-    | (getOpcode inst) == 0x13 && (getFunct3 inst) == 3 = (Just $ Sltiu (getRd inst) (getRs1 inst) (getImm12 inst), rest)
+newDecode bytes@(b0:b1:b2:b3:_)
+    | (getOpcode inst) == 0x13 && (getFunct3 inst) == 0 = Just $ NewAddi 4 (getRd inst) (getRs1 inst) (getImm12 inst)
+    | (getOpcode inst) == 0x13 && (getFunct3 inst) == 2 = Just $ NewSlti 4 (getRd inst) (getRs1 inst) (getImm12 inst)
+    | (getOpcode inst) == 0x13 && (getFunct3 inst) == 3 = Just $ NewSltiu 4 (getRd inst) (getRs1 inst) (getImm12 inst)
   where
-    inst = (combineBytes [b0, b1, b2, b3])
+    inst = (combineBytes $ take 4 bytes)
 
-newDecode rest = (Nothing, rest)
+newDecode _ = Nothing
 
-decode32 inst = fromJust $ fst $ newDecode (splitWord inst)
+decode32 inst = fromJust $ newDecode (splitWord inst)
 
 
 -----------------------------------------
@@ -34,4 +40,5 @@ main = do
   putStrLn $ show $ decode inst_integer
   putStrLn $ show $ decode32 inst_integer
   putStrLn $ show $ newDecode inst_bytes
+  putStrLn $ show $ newDecode [0x13, 0x05, 0x15]
 
